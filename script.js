a5e0217c9e0365b5ca0599a9794b406bc2f5fb7a
Glitch (hello-webpage) <none>
2019-01-14 19:54:48 +0000

    ☂️🍪 Checkpoint ./index.html:2265073/196 ./script.js:2265073/607 ./style.css:2265073/4

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ const callback = function(err, data) {
     alert('Something went wrong: ' + err);
   } else {
     console.log(data); 
+    
+    
+     d3.select("body")
+      .append("h1")
+      .attr("id", "title")
+      .text("Monthly Global Land-Surface Temperature");
+    
+    d3.select("body")
+      .append("h2")
+      .text("1753 - 2015: base temperature 8.66℃");
+    
+    d3.select("body")
+      .append("div")
+      .attr("id", "description")
+      .text("1753 - 2015: base temperature 8.66℃");
+    
   }
 }
 xhr.open('GET', 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json', true);
